a78313975dabf72e6f1aa1ab45034c47658ee76d
Admin <admin@ptr-g7fkvf0j2jc7ezmzlbo.18120a2.ip6.access.telenet.be>
2017-05-26 19:14:35 +0300

    invoegen data in cell voor table

--- Werkstuk1/FirstViewController.swift
+++ Werkstuk1/FirstViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let voornamen = ["geordy", "sara", "test"]
+    let achternamen = ["van cutsem", "van cutsem", "klein"]
+    
+    @IBOutlet var tableView: UITableView!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return voornamen.count
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
+    {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "contactIdentifier");
+        
+        
+        
+        return UITableViewCell();
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
